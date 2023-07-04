4328778168b1e7b015fa935410ca7e90fe2529fd
Gamaliel Adun <adungamaliel@gmail.com>
2023-07-04 19:52:22 +0000

    added setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,4 @@
+-- mysql script to setup the user and db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED WITH mysql_native_password BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
