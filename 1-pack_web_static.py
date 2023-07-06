c05d5ef10311a886eabf81f25e80ba8fbd3807ef
Gamaliel Adun <adungamaliel@gmail.com>
2023-07-06 09:44:26 +0000

    added 1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,15 +8,15 @@ import os
 
 def do_pack():
     """
-    This is a function that packs the contents of 
+    This is a function that packs the contents of
     folder web_static
-    
+
     args:
         None
-        
+
     Return:
         None if function failed
-        else, returns 
+        else, returns
     """
 
     if not os.path.exists("versions"):
@@ -33,6 +33,6 @@ def do_pack():
         local(f"tar -cvzf {arc_path} {dir}")
         size = os.stat(arc_path)
         print(f"{dir} packed: {arc_path} -> {size.st_size}Bytes")
-        return(f"{arc_path}")
+        return f"{arc_path}"
     except Exception as e:
         return None
