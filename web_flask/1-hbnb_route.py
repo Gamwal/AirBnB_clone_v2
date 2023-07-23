94e4ff0a0e53426721da11bc02f5ead49ecf39b9
Gamaliel Adun <adungamaliel@gmail.com>
2023-07-23 23:16:09 +0000

    added 1

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Flask Application"""
+
+from flask import Flask, request
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Function that return Hello HBNB"""
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def dhbnb():
+    """Function that displays HBNB"""
+    return "HBNB"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
